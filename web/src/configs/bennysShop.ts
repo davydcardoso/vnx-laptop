526c44e956b5191c7dfb8b282ab3b9fb7026816f
Davyd Cardoso <davydkewen@gmail.com>
2023-11-27 12:04:38 -0300

    feat: add nitrous and tracker in bennys shop

--- web/src/configs/bennysShop.ts
+++ web/src/configs/bennysShop.ts
@@ -1,4 +1,5 @@
 import RacingHarness from "../assets/itens/harness.png";
+import NitrousOxide from "../assets/itens/nitrous_oxide.png";
 import PhoneDongleRacing from "../assets/itens/racing_usb_blue.png";
 
 export interface ItemShopDTO {
@@ -32,6 +33,20 @@ export const BENNYS_SHOP_ITEMS: BENNYS_SHOP_ITEMS_DTO = {
       stock: 1,
       price: 35,
       photo: RacingHarness,
+    },
+    {
+      id: "trackerdisabler",
+      name: "Tracker Disabler",
+      price: 50,
+      stock: 1,
+      photo: "https://i.imgur.com/hTCokqn.png",
+    },
+    {
+      id: "nitrous",
+      name: "Nitrous Oxide",
+      stock: 1,
+      price: 75,
+      photo: NitrousOxide,
     },
   ],
 };
